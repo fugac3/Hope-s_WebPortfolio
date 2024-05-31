c6d582c4f989a8006329159ea8b4ebe933acc4de
Hope <k.hope.ing@gmail.com>
2024-05-31 19:13:21 +0200

    Changed Theme and Transparent Colour-Inverted Hover

--- Hope's_Portfolio_2024/index.js
+++ Hope's_Portfolio_2024/index.js
@@ -1,5 +1,6 @@
 const navToggle = document.querySelector(".nav_toggle");
 const navLinks = document.querySelectorAll(".nav_link");
+var autoType = document.querySelector(".auto-type");
 
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav_open");
@@ -9,4 +10,11 @@ navLinks.forEach((link) => {
   link.addEventListener("click", () => {
     document.body.classList.remove("nav_open");
   });
+});
+
+// Hover Circle
+document.addEventListener("mousemove", function (e) {
+  const hoverCircle = document.getElementById("hoverCircle");
+  hoverCircle.style.left = e.pageX + "px"; // Move the circle to the current mouse position horizontally
+  hoverCircle.style.top = e.pageY + "px"; // Move the circle to the current mouse position vertically
 });
